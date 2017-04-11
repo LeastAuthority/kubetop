b0907bc5ffdf33dc75c447fbbd048b9bb07e4a0d
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-04-11 13:19:31 -0400

    Collect and render information to do with nodes

--- src/kubetop/test/test_textrenderer.py
+++ src/kubetop/test/test_textrenderer.py
@@ -13,7 +13,10 @@ from bitmath import Byte
 
 from .._textrenderer import (
     _render_memory, _render_container, _render_containers, _render_pods,
+    _render_nodes,
 )
+
+from txkube import v1
 
 
 class RenderMemoryTests(TestCase):
@@ -175,4 +178,163 @@ class PodTests(TestCase):
         self.assertEqual(
             ["bar", "foo"],
             list(line.split()[0].strip() for line in lines),
+        )
+
+
+class NodeTests(TestCase):
+    def test_render_several(self):
+        node = {
+            "kind":"Node",
+            "apiVersion":"v1",
+            "metadata":{
+                "name":"ip-172-20-86-0.ec2.internal",
+                "selfLink":"/api/v1/nodesip-172-20-86-0.ec2.internal",
+                "uid":"e35442ce-0e45-11e7-acfa-1272db66581c",
+                "resourceVersion":"17592039",
+                "creationTimestamp":"2017-03-21T14:51:38Z",
+                "labels":{
+                    "beta.kubernetes.io/arch":"amd64",
+                    "beta.kubernetes.io/instance-type":"m3.medium",
+                    "beta.kubernetes.io/os":"linux",
+                    "failure-domain.beta.kubernetes.io/region":"us-east-1",
+                    "failure-domain.beta.kubernetes.io/zone":"us-east-1b",
+                    "kubernetes.io/hostname":"ip-172-20-86-0.ec2.internal",
+                },
+                "annotations":{
+                    "volumes.kubernetes.io/controller-managed-attach-detach":"true",
+                },
+            },
+            "spec":{
+                "podCIDR":"100.96.1.0/24",
+                "externalID":"i-0dd2f62f32659dcb2",
+                "providerID":"aws:///us-east-1b/i-0dd2f62f32659dcb2",
+            },
+            "status":{
+                "capacity":{
+                    "alpha.kubernetes.io/nvidia-gpu":"0",
+                    "cpu":"1",
+                    "memory":"3857324Ki",
+                    "pods":"110",
+                },
+                "allocatable":{
+                    "alpha.kubernetes.io/nvidia-gpu":"0",
+                    "cpu":"1",
+                    "memory":"200Ki",
+                    "pods":"110",
+                },
+                "conditions":[
+                    {
+                        "type":"OutOfDisk",
+                        "status":"False",
+                        "lastHeartbeatTime":"2017-04-07T22:56:28Z",
+                        "lastTransitionTime":"2017-03-21T14:51:38Z",
+                        "reason":"KubeletHasSufficientDisk",
+                        "message":"kubelet has sufficient disk space available",
+                    },
+                    {
+                        "type":"MemoryPressure",
+                        "status":"False",
+                        "lastHeartbeatTime":"2017-04-07T22:56:28Z",
+                        "lastTransitionTime":"2017-03-21T14:51:38Z",
+                        "reason":"KubeletHasSufficientMemory",
+                        "message":"kubelet has sufficient memory available",
+                    },
+                    {
+                        "type":"DiskPressure",
+                        "status":"False",
+                        "lastHeartbeatTime":"2017-04-07T22:56:28Z",
+                        "lastTransitionTime":"2017-03-21T14:51:38Z",
+                        "reason":"KubeletHasNoDiskPressure",
+                        "message":"kubelet has no disk pressure",
+                    },
+                    {
+                        "type":"Ready",
+                        "status":"True",
+                        "lastHeartbeatTime":"2017-04-07T22:56:28Z",
+                        "lastTransitionTime":"2017-03-21T14:52:08Z",
+                        "reason":"KubeletReady",
+                        "message":"kubelet is posting ready status",
+                    },
+                    {
+                        "type":"NetworkUnavailable",
+                        "status":"False",
+                        "lastHeartbeatTime":"2017-03-21T14:51:43Z",
+                        "lastTransitionTime":"2017-03-21T14:51:43Z",
+                        "reason":"RouteCreated",
+                        "message":"RouteController created a route",
+                    },
+                ],
+                "addresses":[
+                    {"type":"InternalIP","address":"172.20.86.0"},
+                    {"type":"LegacyHostIP","address":"172.20.86.0"},
+                    {"type":"ExternalIP","address":"54.82.248.124"},
+                    {"type":"Hostname","address":"ip-172-20-86-0.ec2.internal"},
+                ],
+                "daemonEndpoints":{
+                    "kubeletEndpoint":{"Port":10250},
+                },
+                "nodeInfo":{
+                    "machineID":"1af1476dd91d40c0952ff71f54297123",
+                    "systemUUID":"EC28F27B-6B13-F91A-79BF-DFC0EA0B0BBD",
+                    "bootID":"a4d005b1-3117-4989-8a56-37d831d66d9d",
+                    "kernelVersion":"4.4.26-k8s",
+                    "osImage":"Debian GNU/Linux 8 (jessie)",
+                    "containerRuntimeVersion":"docker://1.12.3",
+                    "kubeletVersion":"v1.5.2",
+                    "kubeProxyVersion":"v1.5.2",
+                    "operatingSystem":"linux",
+                    "architecture":"amd64",
+                },
+                "images":[
+                    {
+                        "names":[
+                            "example.invalid/foo@sha256:1685a4543dc70cb29e5a9df4b47a09ed7d6e54c00fb50296afff65683c67e0ff",
+                            "example.invalid/foo:1941d38",
+                        ],
+                        "sizeBytes":752076313,
+                    },
+                ],
+                "volumesInUse":[
+                    "kubernetes.io/aws-ebs/vol-01b01d11a6b17e2de",
+                    "kubernetes.io/aws-ebs/aws://us-east-1b/vol-0b977509f3c44d901",
+                    "kubernetes.io/aws-ebs/aws://us-east-1b/vol-091a8106ddd94357b",
+                    "kubernetes.io/aws-ebs/vol-0e80ac26be3edd63f",
+                ],
+                "volumesAttached":[
+                    {"name":"kubernetes.io/aws-ebs/vol-01b01d11a6b17e2de","devicePath":"/dev/xvdbc"},
+                    {"name":"kubernetes.io/aws-ebs/aws://us-east-1b/vol-091a8106ddd94357b","devicePath":"/dev/xvdba"},
+                    {"name":"kubernetes.io/aws-ebs/vol-0e80ac26be3edd63f","devicePath":"/dev/xvdbb"},
+                    {"name":"kubernetes.io/aws-ebs/aws://us-east-1b/vol-0b977509f3c44d901","devicePath":"/dev/xvdbd"},
+                ],
+            },
+        }
+
+        usage = {
+            "metadata": {
+                "name": "ip-172-20-86-0.ec2.internal",
+                "creationTimestamp": "2017-04-11T14:41:47Z"
+            },
+            "timestamp": "2017-04-11T14:41:00Z",
+            "window": "1m0s",
+            "usage": {
+                "cpu": "68m",
+                "memory": "100Ki"
+            }
+        }
+
+        pods = [
+            v1.Pod(
+                status=v1.PodStatus(
+                    hostIP=node["status"]["addresses"][0]["address"],
+                ),
+            ),
+        ]
+
+        self.assertEqual(
+            "Node 0 "
+            "CPU%  6.80 "
+            "MEM% 50.00 ( 100 KiB/ 200 KiB)  "
+            "POD%  0.91 (  1/110) "
+            "Ready\n",
+            _render_nodes([node], [usage], pods),
         )
