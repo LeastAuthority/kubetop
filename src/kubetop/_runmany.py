56cdf51b2ff154ad602f9532b6fb552799c3eda2
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-04-11 08:56:29 -0400

    Simplify _runmany a bit.  Also docs.

--- src/kubetop/_runmany.py
+++ src/kubetop/_runmany.py
@@ -13,51 +13,52 @@ Theory of Operation
 #. Deliver rendering success/failure to the main object for reporting and exit.
 """
 
-from twisted.python.failure import Failure
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import deferLater
-from twisted.application.service import Service
 
+from ._runonce import run_once_service
 
 @inlineCallbacks
-def _iterate(main, reactor, intervals, f):
+def _iterate(reactor, intervals, f):
+    """
+    Run a function repeatedly.
+
+    :param reactor: See ``run_many_service``.
+
+    :return Deferred: A deferred which fires when ``f`` fails or when
+        ``intervals`` is exhausted.
+    """
     while True:
         before = reactor.seconds()
-        try:
-            yield f()
-        except:
-            main.exit(Failure())
-            break
+        yield f()
         after = reactor.seconds()
         try:
             interval = next(intervals)
         except StopIteration:
-            main.exit()
             break
         delay = max(0, interval - (after - before))
         yield deferLater(reactor, delay, lambda: None)
 
 
 
-class _RunManyService(Service):
-    def startService(self):
-        Service.startService(self)
-        # Services can get started before the reactor has started.  This
-        # complicates things.  Get rid of all that complexity but not running
-        # any interesting application code until the reactor has actually
-        # started.
-        self.reactor.callWhenRunning(self._run_until_error)
-
-
-    def _run_until_error(self):
-        _iterate(self.main, self.reactor, self.intervals, self.f)
-
-
-
 def run_many_service(main, reactor, f, intervals):
-    s = _RunManyService()
-    s.main = main
-    s.reactor = reactor
-    s.f = f
-    s.intervals = intervals
-    return s
+    """
+    Create a service to run a function repeatedly.
+
+    :param TwistMain main: The main entrypoint object.
+
+    :param reactor: The IReactorTime & IReactorCore provider to use for
+        delaying subsequent iterations.
+
+    :param intervals: An iterable of numbers giving the delay between
+        subsequent invocations of the function.
+
+    :param f: A zero-argument callable to repeatedly call.
+
+    :return IService: A service which will run the function until the interval
+        iterator is exhausted.
+    """
+    def run_many():
+        return _iterate(reactor, intervals, f)
+
+    return run_once_service(main, reactor, run_many)
